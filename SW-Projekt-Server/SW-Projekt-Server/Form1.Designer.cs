a2b14f59ae1df806ef05d34dcd40eaa877ac610f
Florian <Florian@DESKTOP-IE3B4DR>
2018-12-10 16:59:35 +0100

    Kreutzer_Zeugs.cs für DB Übertragung

--- SW-Projekt-Server/SW-Projekt-Server/Form1.Designer.cs
+++ SW-Projekt-Server/SW-Projekt-Server/Form1.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(136, 166);
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -43,10 +44,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(120, 41);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "WatchDog-Server";
             this.ResumeLayout(false);
---
+++ SW-Projekt-Server/SW-Projekt-Server/Kreutzers_Zeugs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace SW_Projekt_Server
+{
+    class Kreutzers_Zeugs
+    {
+        public static void Main()
+        {
+            string IP = "";
+            string connString = "server=127.0.0.1;user=root;database=mir_pups_egal;port=3306;password=Martin123";
+            MySqlConnection conn = new MySqlConnection(connString);
+            try
+            {
+                //Console.WriteLine("Connecting to MySQL...");
+                MessageBox.Show("Connecting to the Database");
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.ToString());
+                MessageBox.Show(ex.ToString());
+            }
+            conn.Close();
+            //Console.WriteLine("Done.");
+            MessageBox.Show("Done");
+            conn.Open();
+            string query2 = "INSERT INTO fehlfunktion (Art) VALUE (@errorcode)";
+            MySqlCommand cmd = new MySqlCommand(query2, conn);
+            cmd.Parameters.AddWithValue("@errorcode", IP);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
