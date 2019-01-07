dde5f56e746f5f7233fd7d6e32417350a1f9d227
Florian <Florian@DESKTOP-IE3B4DR>
2019-01-07 14:08:11 +0100

    Update

--- SW-Projekt-Server/SW-Projekt-Server/Kreutzers_Zeugs.cs
+++ SW-Projekt-Server/SW-Projekt-Server/Kreutzers_Zeugs.cs
@@ -7,36 +7,39 @@ using System.Data;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using System.Windows.Forms;
-using System.Text.RegularExpressions;
 namespace SW_Projekt_Server
 {
     class Kreutzers_Zeugs
     {
         public static void Main()
         {
-            string IP = "";
-            string connString = "server=127.0.0.1;user=root;database=mir_pups_egal;port=3306;password=Martin123";
+            //---------------------------------------------------------------!!!WICHTIG FÜR DICH GREGS!!!---------------------------------------------------------------
+            string IP = ""; // IP adresse vom Gregs zum eintragen in die Datenbank
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+            string connString = "server=127.0.0.1;user=root;database=mir_pups_egal;port=3306;password=Martin123"; //Datenbankparameter also wohin man Verbinden soll und welcher login
             MySqlConnection conn = new MySqlConnection(connString);
+            //-------------------------------------------------------------------------------------------Test Anfang
             try
             {
-                //Console.WriteLine("Connecting to MySQL...");
                 MessageBox.Show("Connecting to the Database");
-                conn.Open();
+                conn.Open();//Versuche die Verbindung zu öffnen
             }
             catch (Exception ex)
             {
-                //Console.WriteLine(ex.ToString());
-                MessageBox.Show(ex.ToString());
+                MessageBox.Show(ex.ToString()); //Fehlercode Ausgabe falls keine Verbindung hergestellt werden kann
             }
             conn.Close();
-            //Console.WriteLine("Done.");
             MessageBox.Show("Done");
+            //-------------------------------------------------------------------------------------------Test Ende
             conn.Open();
-            string query2 = "INSERT INTO fehlfunktion (Art) VALUE (@errorcode)";
+            string query2 = "INSERT INTO fehlfunktion (Art) VALUE (@errorcode)"; //Befehl für die Datenbank
             MySqlCommand cmd = new MySqlCommand(query2, conn);
-            cmd.Parameters.AddWithValue("@errorcode", IP);
-            cmd.ExecuteNonQuery();
-            conn.Close();
+            cmd.Parameters.AddWithValue("@errorcode", IP); // Die IP adresse die wir vom Gregs weitergeben wollen
+            cmd.ExecuteNonQuery();//Ausführen des Befehls
+            conn.Close(); //Verbindung unterbrechen wegen Netzwerkauslastung
         }
     }
 }
